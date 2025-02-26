4ed4e95488c61c86d2d62d88a808095fc08a8b09
eshta <eshtadevatha06@gmail.com>
2025-02-26 22:46:07 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Keep this for local
+    strictPort: true,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: process.env.PORT || 3000, // Ensure it binds to Render's assigned port
+    strictPort: true,
+    host: '0.0.0.0',
+  },
+});
