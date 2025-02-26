2c074163bb23bf6a8e9b511ce6aeae3b6d762fd9
eshta <eshtadevatha06@gmail.com>
2025-02-26 22:41:21 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000, // Fallback for local testing
-    strictPort: true, // Ensures it binds only to the given port
-    host: '0.0.0.0',  // Allows external access (important for Render)
-  },
-  preview: {
-    port: process.env.PORT || 3000,
-    strictPort: true,
-    host: '0.0.0.0',
-    allowedHosts: ['react-weather-app-v456.onrender.com'], // Replace with your actual Render domain
-  },
-});
-
-
-
-
-
-
-
+})
